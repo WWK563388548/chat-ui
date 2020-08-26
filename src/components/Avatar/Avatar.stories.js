a965b08da9da9a5a9326ac41b4284d228b3146da
Weikai Wang <wc563388548@gmail.com>
2020-08-26 22:54:10 +0900

    Add story for Avatar component

---
+++ src/components/Avatar/Avatar.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Avatar from '.';
+import Image from '../../assets/images/image.png';
+import '../../story.css';
+
+export default {
+  title: 'Avatar',
+  component: Avatar,
+}
+
+export const Default = () => {
+  return <Avatar imageSrc={Image} />;
+}
+
+export const Sizes = () => {
+  return (
+    <div className='row-element'>
+      <Avatar imageSrc={Image} />
+      <Avatar imageSrc={Image} size='56px' />
+      <Avatar imageSrc={Image} size='64px' />
+      <Avatar imageSrc={Image} size='72px' />
+    </div>
+  )
+}
+
+export const withStatus = () => {
+  return (
+    <div className='row-element'>
+      <Avatar imageSrc={Image} status='online' />
+      <Avatar imageSrc={Image} status='offline' />
+      <Avatar imageSrc={Image} size='72px' status='offline' statusIconSize='12px' />
+    </div>
+  )
+}
