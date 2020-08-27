f9ffdec621bc747809d024add9badf5c3bcbce45
Weikai Wang <wc563388548@gmail.com>
2020-08-27 23:43:00 +0900

    Add Badge component to storybook

---
+++ src/components/Badge/Badge.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Badge from '.';
+import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default {
+  title: 'Basic UI/Badge',
+  component: Badge,
+}
+
+export const Default = () => {
+  return <Badge count={5} />;
+}
+
+export const DotVariant = () => {
+  return (
+    <Badge show variant='dot'>
+      <FontAwesomeIcon icon={faCommentDots} style={{ fontSize: '24px' }} />;
+    </Badge>
+  );
+}
