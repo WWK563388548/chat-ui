aa8fc2718f192283c7865b5829472a65e8722b4d
Weikai Wang <wc563388548@gmail.com>
2020-08-27 23:37:28 +0900

    Using fontAwesome library in Icon storybook

--- src/components/Icon/Icon.stories.js
+++ src/components/Icon/Icon.stories.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import Icon from '.';
+
+import {
+  faCommentDots,
+  faFolder,
+  faStickyNote,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import SmileIconSvg from '../../assets/icons/smile.svg';
+
+/**
+ * fontawesome 图标库安装命令
+ * yarn add @fortawesome/react-fontawesome 
+ * @fortawesome/fontawesome-svg-core 
+ * @fortawesome/free-brands-svg-icons 
+ * @fortawesome/free-regular-svg-icons 
+ * @fortawesome/free-solid-svg-icons
+*/
 
 export default {
   title: 'Basic UI/Icon',
@@ -23,3 +40,22 @@ export const CustomSize = () => {
 export const CustomOpacity = () => {
   return <Icon IconComponent={SmileIconSvg} opacity={0.7} />;
 }
+
+// 使用FontAwesome图标库
+export const FontAwesome = () => {
+  return <FontAwesomeIcon icon={faCommentDots} />;
+};
+
+export const FontAwesomeColor = () => {
+  return <FontAwesomeIcon icon={faCommentDots} style={{ color: "#cccccc" }} />;
+};
+
+export const FontAwesomeSizes = () => {
+  return (
+    <div className='row-element'>
+      <FontAwesomeIcon icon={faFolder} style={{ fontSize: "24px" }} />
+      <FontAwesomeIcon icon={faStickyNote} style={{ fontSize: "36px" }} />
+      <FontAwesomeIcon icon={faCommentDots} style={{ fontSize: "48px" }} />
+    </div>
+  );
+};
