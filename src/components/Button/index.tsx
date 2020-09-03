5da111e19de3ec81ba142c4728b5efce423e56b7
Weikai Wang <wc563388548@gmail.com>
2020-09-03 13:58:24 +0900

    adding button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ButtonContainer } from './style';
+
+interface Props {
+  children: any
+  buttonType?: string
+  shape?: 'circle' | 'rect'
+  size?: string
+  bgColor?: string
+}
+
+const Heading: React.SFC<Props> = ({ children, buttonType = 'primary', shape = 'circle', size = '30px', bgColor }) => {
+  return (
+    <ButtonContainer 
+      buttonType={buttonType}
+      shape={shape}
+      size={size}
+      bgColor={bgColor}
+    >
+      { children }
+    </ButtonContainer>
+  );
+}
+
+export default Heading;
